ea10938ce2417ca6f476e0f7def7eb7a8ca68725
Abhay Dandge <abhay.d.patil96@gmail.com>
2026-06-12 12:47:37 +0530

    Update collect-results.sh

--- kube-hunter/scripts/collect-results.sh
+++ kube-hunter/scripts/collect-results.sh
@@ -2,6 +2,6 @@
 
 POD=$(kubectl get pods -n kube-hunter | grep kube-hunter | awk '{print $1}')
 
-kubectl logs $POD -n kube-hunter > ../reports/scan-report.txt
+kubectl logs $POD -n kube-hunter > ./reports/scan-report.txt
 
 echo "Results saved to reports/scan-report.txt"
