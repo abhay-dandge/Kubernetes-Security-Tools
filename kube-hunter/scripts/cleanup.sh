3d56c064822714e26e61aa1757c4ccb21eec5b74
Abhay Dandge <abhay.d.patil96@gmail.com>
2026-06-12 13:00:02 +0530

    Update cleanup.sh

--- kube-hunter/scripts/cleanup.sh
+++ kube-hunter/scripts/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-kubectl delete -f ../manifests/job.yaml
+kubectl delete -f ./manifests/job.yaml
 
-kubectl delete -f ../manifests/namespace.yaml
+kubectl delete -f ./manifests/namespace.yaml
 
 echo "Cleanup Complete"
