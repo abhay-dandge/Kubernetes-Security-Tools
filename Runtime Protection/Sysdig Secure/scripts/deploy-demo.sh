d00bbefaf52b909209922d46906ed5f99f2de5ed
Abhay Dandge <abhay.d.patil96@gmail.com>
2026-06-12 19:07:26 +0530

    Update deploy-demo.sh

--- Runtime Protection/Sysdig Secure/scripts/deploy-demo.sh
+++ Runtime Protection/Sysdig Secure/scripts/deploy-demo.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+kubectl apply -f ./manifests/namespace.yaml
+
+kubectl apply -f ./manifests/nginx-deployment.yaml
+
+kubectl apply -f ./manifests/suspicious-pod.yaml
+
+echo "Demo Environment Created"
